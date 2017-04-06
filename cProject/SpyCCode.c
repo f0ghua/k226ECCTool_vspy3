f008d7c1a9e187a31bedc5ac188760cc2116c3f8
f0ghua <fog.hua@gmail.com>
2017-04-06 11:14:33 +0800

    * fix bug that ecuid doesn't translate bcd to ascii

--- cProject/SpyCCode.c
+++ cProject/SpyCCode.c
@@ -90,7 +90,7 @@ void Spy_Main()
 char g_str_appSig_mpiWrite_MTC[MPI_STRLEN] = {0};
 void SpyAppSig_AS_appSig_mpiWrite_MTC(double dNewValue)
 {
-   // TODO: Add Event Code
+    // TODO: Add Event Code
 	int i = 0;
 	char text[MPI_STRLEN];
 
@@ -98,7 +98,7 @@ void SpyAppSig_AS_appSig_mpiWrite_MTC(double dNewValue)
 	if (strcmp(g_str_appSig_mpiWrite_MTC, text)) {
 		memset(g_str_appSig_mpiWrite_MTC, 0, sizeof(g_str_appSig_mpiWrite_MTC));
 		strncpy_s(g_str_appSig_mpiWrite_MTC, sizeof(g_str_appSig_mpiWrite_MTC),
-			text, strlen(text));
+                  text, strlen(text));
 		for (i = 0; i < MPI_MTC_LENGTH; i++) {
 			AS_appSig_mpiWrite_bytesMTC_SetAt(i, g_str_appSig_mpiWrite_MTC[i]);
 		}
@@ -108,7 +108,7 @@ void SpyAppSig_AS_appSig_mpiWrite_MTC(double dNewValue)
 char g_str_appSig_mpiWrite_BMPNAC[MPI_STRLEN] = {0};
 void SpyAppSig_AS_appSig_mpiWrite_BMPNAC(double dNewValue)
 {
-   // TODO: Add Event Code
+    // TODO: Add Event Code
 	int i = 0;
 	char text[MPI_STRLEN];
 
@@ -116,7 +116,7 @@ void SpyAppSig_AS_appSig_mpiWrite_BMPNAC(double dNewValue)
 	if (strcmp(g_str_appSig_mpiWrite_BMPNAC, text)) {
 		memset(g_str_appSig_mpiWrite_BMPNAC, 0, sizeof(g_str_appSig_mpiWrite_BMPNAC));
 		strncpy_s(g_str_appSig_mpiWrite_BMPNAC, sizeof(g_str_appSig_mpiWrite_BMPNAC),
-			text, strlen(text));
+                  text, strlen(text));
 		for (i = 0; i < sizeof(g_str_appSig_mpiWrite_BMPNAC); i++) {
 			AS_appSig_mpiWrite_bytesBMPNAC_SetAt(i, g_str_appSig_mpiWrite_BMPNAC[i]);
 		}
@@ -126,7 +126,7 @@ void SpyAppSig_AS_appSig_mpiWrite_BMPNAC(double dNewValue)
 char g_str_appSig_mpiWrite_EMPNAC[MPI_STRLEN] = {0};
 void SpyAppSig_AS_appSig_mpiWrite_EMPNAC(double dNewValue)
 {
-   // TODO: Add Event Code
+    // TODO: Add Event Code
 	int i = 0;
 	char text[MPI_STRLEN];
 
@@ -134,7 +134,7 @@ void SpyAppSig_AS_appSig_mpiWrite_EMPNAC(double dNewValue)
 	if (strcmp(g_str_appSig_mpiWrite_EMPNAC, text)) {
 		memset(g_str_appSig_mpiWrite_EMPNAC, 0, sizeof(g_str_appSig_mpiWrite_EMPNAC));
 		strncpy_s(g_str_appSig_mpiWrite_EMPNAC, sizeof(g_str_appSig_mpiWrite_EMPNAC),
-			text, strlen(text));
+                  text, strlen(text));
 		for (i = 0; i < sizeof(g_str_appSig_mpiWrite_EMPNAC); i++) {
 			AS_appSig_mpiWrite_bytesEMPNAC_SetAt(i, g_str_appSig_mpiWrite_EMPNAC[i]);
 		}
@@ -144,77 +144,103 @@ void SpyAppSig_AS_appSig_mpiWrite_EMPNAC(double dNewValue)
 char g_str_appSig_mpiRead_MTC[MPI_STRLEN] = {0};
 void SpyAppSig_AS_appSig_mpiRead_bytesMTC(double dNewValue)
 {
-   // TODO: Add Event Code
-   int i = 0;
+    // TODO: Add Event Code
+    int i = 0;
    
-   for (i = 0; i < MPI_MTC_LENGTH; i++) {
-       g_str_appSig_mpiRead_MTC[i] = (char)AS_appSig_mpiRead_bytesMTC_GetAt(i);
-   }
+    for (i = 0; i < MPI_MTC_LENGTH; i++) {
+        g_str_appSig_mpiRead_MTC[i] = (char)AS_appSig_mpiRead_bytesMTC_GetAt(i);
+    }
    
-   AS_appSig_mpiRead_MTC_SetText(g_str_appSig_mpiRead_MTC);
+    AS_appSig_mpiRead_MTC_SetText(g_str_appSig_mpiRead_MTC);
 }
 
 char g_str_appSig_mpiRead_EMPNAC[MPI_STRLEN] = {0};
 void SpyAppSig_AS_appSig_mpiRead_bytesEMPNAC(double dNewValue)
 {
-   // TODO: Add Event Code
-   int i = 0;
+    // TODO: Add Event Code
+    int i = 0;
    
-   for (i = 0; i < MPI_EMPNAC_LENGTH; i++) {
+    for (i = 0; i < MPI_EMPNAC_LENGTH; i++) {
 		g_str_appSig_mpiRead_EMPNAC[i] = (char)AS_appSig_mpiRead_bytesEMPNAC_GetAt(i);
-   }
+    }
    
-   AS_appSig_mpiRead_EMPNAC_SetText(g_str_appSig_mpiRead_EMPNAC);
+    AS_appSig_mpiRead_EMPNAC_SetText(g_str_appSig_mpiRead_EMPNAC);
 }
 
 char g_str_appSig_mpiRead_BMPNAC[MPI_STRLEN] = {0};
 void SpyAppSig_AS_appSig_mpiRead_bytesBMPNAC(double dNewValue)
 {
-   // TODO: Add Event Code
-   int i = 0;
+    // TODO: Add Event Code
+    int i = 0;
    
-   for (i = 0; i < MPI_BMPNAC_LENGTH; i++) {
+    for (i = 0; i < MPI_BMPNAC_LENGTH; i++) {
 		g_str_appSig_mpiRead_BMPNAC[i] = (char)AS_appSig_mpiRead_bytesBMPNAC_GetAt(i);
-   }
+    }
    
-   AS_appSig_mpiRead_BMPNAC_SetText(g_str_appSig_mpiRead_BMPNAC);
+    AS_appSig_mpiRead_BMPNAC_SetText(g_str_appSig_mpiRead_BMPNAC);
+}
+
+static int bcd2Ascii(char src, char *dest)
+{
+    char outputs[] = "0123456789ABCDEF";
+    *dest++ = outputs[src>>4];
+    *dest++ = outputs[src&0x0F];
+    *dest = '\0';
+
+    return 0;
 }
 
 char g_str_appSig_mpiRead_ECUID[MPI_STRLEN] = {0};
 void SpyAppSig_AS_appSig_mpiRead_bytesECUID(double dNewValue)
 {
-   // TODO: Add Event Code
-   int i = 0;
+	// TODO: Add Event Code
+	int i = 0;
+	int offset = 8;
+	int j = offset;
+	char bcd, asciiCodes[3];
    
-   for (i = 0; i < (MPI_ECUID_LENGTH-8); i++) {
-		g_str_appSig_mpiRead_ECUID[i] = (char)AS_appSig_mpiRead_bytesECUID_GetAt(i+8);
-   }
+	// g_str_appSig_mpiRead_ECUID only stores content from byte8, since byte13 is BCD code, which 
+	// expand to 2 bytes ASCII code, so total 9 bytes in array
+	// 
+	for (i = 0; i < 5; i++) {
+		g_str_appSig_mpiRead_ECUID[i] = (char)AS_appSig_mpiRead_bytesECUID_GetAt(j);
+		j++;
+	}
+
+	bcd = (char)AS_appSig_mpiRead_bytesECUID_GetAt(j++);
+	bcd2Ascii(bcd, asciiCodes);
+
+	g_str_appSig_mpiRead_ECUID[i++] = asciiCodes[0];
+	g_str_appSig_mpiRead_ECUID[i++] = asciiCodes[1];
    
-   AS_appSig_mpiRead_ECUID_SetText(g_str_appSig_mpiRead_ECUID);
+	g_str_appSig_mpiRead_ECUID[i++] = (char)AS_appSig_mpiRead_bytesECUID_GetAt(j++);
+	g_str_appSig_mpiRead_ECUID[i++] = (char)AS_appSig_mpiRead_bytesECUID_GetAt(j++);
+
+	AS_appSig_mpiRead_ECUID_SetText(g_str_appSig_mpiRead_ECUID);
 }
 
 char g_str_appSig_mpiRead_VPPS[MPI_STRLEN] = {0};
 void SpyAppSig_AS_appSig_mpiRead_bytesVPPS(double dNewValue)
 {
-   // TODO: Add Event Code
-   int i = 0;
+    // TODO: Add Event Code
+    int i = 0;
    
-   for (i = 0; i < MPI_VPPS_LENGTH; i++) {
+    for (i = 0; i < MPI_VPPS_LENGTH; i++) {
 		g_str_appSig_mpiRead_VPPS[i] = (char)AS_appSig_mpiRead_bytesVPPS_GetAt(i);
-   }
+    }
    
-   AS_appSig_mpiRead_VPPS_SetText(g_str_appSig_mpiRead_VPPS);
+    AS_appSig_mpiRead_VPPS_SetText(g_str_appSig_mpiRead_VPPS);
 }
 
 char g_str_appSig_mpiRead_DUNS[MPI_STRLEN] = {0};
 void SpyAppSig_AS_appSig_mpiRead_bytesDUNS(double dNewValue)
 {
-   // TODO: Add Event Code
-   int i = 0;
+    // TODO: Add Event Code
+    int i = 0;
    
-   for (i = 0; i < MPI_DUNS_LENGTH; i++) {
+    for (i = 0; i < MPI_DUNS_LENGTH; i++) {
 		g_str_appSig_mpiRead_DUNS[i] = (char)AS_appSig_mpiRead_bytesDUNS_GetAt(i);
-   }
+    }
    
-   AS_appSig_mpiRead_DUNS_SetText(g_str_appSig_mpiRead_DUNS);
+    AS_appSig_mpiRead_DUNS_SetText(g_str_appSig_mpiRead_DUNS);
 }
